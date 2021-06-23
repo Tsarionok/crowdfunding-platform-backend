3c286e56fd40e80680d04401fa5abd039d886ed2
Sergey Tsarionok <s.tsarionok.vironit@vironit.ru>
2021-06-23 14:31:20 +0300

    add photos repository

---
+++ CrowdfundingPlatform/DataAccessLayer/Repository/IPhotoRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Entity;
+
+namespace DataAccessLayer.Repository
+{
+    public interface IPhotoRepository : ICrudRepository<Photo>
+    {
+    }
+}
