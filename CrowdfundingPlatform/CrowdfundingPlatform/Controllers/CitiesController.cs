39f2a08e6dcd3019ff6581def53882765cd7ff9d
Sergey Tsarionok <s.tsarionok.vironit@vironit.ru>
2021-06-24 19:51:30 +0300

    modify Cities controller

--- CrowdfundingPlatform/CrowdfundingPlatform/Controllers/CitiesController.cs
+++ CrowdfundingPlatform/CrowdfundingPlatform/Controllers/CitiesController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using BusinessLogicLayer.DTO;
 using BusinessLogicLayer.Service;
+using BusinessLogicLayer.Service.Implementation;
+using CrowdfundingPlatform.Models.City;
+using CrowdfundingPlatform.Models.Country;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,62 +17,96 @@ namespace CrowdfundingPlatform.Controllers
     public class CitiesController : ControllerBase
     {
         ICityService _service;
+        ICountryService _countryService;
 
-        public CitiesController(ICityService service)
+        public CitiesController(ICityService service, ICountryService countryService)
         {
             _service = service;
+            _countryService = countryService;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<CityDTO>>> Get()
+        public async Task<ActionResult<ICollection<CityWithCountryModel>>> Get()
         {
-            return Ok(await _service.ReadAll());
+            ICollection<CityWithCountryModel> cities = new List<CityWithCountryModel>();
+            foreach (CityDTO readableCity in await _service.ReadAll())
+            {
+                cities.Add(new CityWithCountryModel
+                {
+                    Id = readableCity.Id,
+                    Name = readableCity.Name,
+                    Country = new CountryModel
+                    {
+                        Id = readableCity.Country.Id,
+                        Name = readableCity.Country.Name
+                    }
+                });
+            }
+            return Ok(cities);
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<CityDTO>> Get(int id)
+        public async Task<ActionResult<CityWithCountryModel>> Get(int id)
         {
-            CityDTO city = _service.ReadById(id).Result;
+            CityDTO readableCity = await _service.ReadById(id);
 
-            if (city == null)
+            if (readableCity == null)
             {
                 return NotFound();
             }
 
-            return Ok(await _service.ReadById(id));
+            return Ok(new CityWithCountryModel
+            {
+                Id = readableCity.Id,
+                Name = readableCity.Name,
+                Country = new CountryModel
+                {
+                    Id = readableCity.Country.Id,
+                    Name = readableCity.Country.Name
+                }
+            });
         }
 
         [HttpPost]
-        public async Task<ActionResult<CityDTO>> Post(CityDTO city)
+        public async Task<ActionResult> Post(CityCreatedModel city)
         {
-            if (city == null)
+            if (city == null || city.Name == "")
             {
                 return BadRequest();
             }
-            await _service.Create(city);
+            await _service.Create(new CityDTO
+            {
+                Name = city.Name,
+                Country = _countryService.ReadAll().Result.Where(c => c.Id == city.CountryId).FirstOrDefault()
+            });
             return Ok(city);
         }
 
         [HttpPut]
-        public async Task<ActionResult> Put(CityDTO city)
+        public async Task<ActionResult> Put(CityUpdatedModel city)
         {
             if (city == null)
             {
                 return NotFound();
             }
-            await _service.Update(city);
+            await _service.Update(new CityDTO
+            {
+                Id = city.Id,
+                Name = city.Name,
+                Country = await _countryService.ReadById(city.CountryId)
+            });
             return Ok();
         }
 
         [HttpDelete("{id}")]
-        public async Task<ActionResult<CityDTO>> Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
             CityDTO city = await _service.DeleteById(id);
             if (city == null)
             {
                 return NotFound();
             }
-            return Ok(city);
+            return Ok();
         }
     }
 }
---
+++ CrowdfundingPlatform/CrowdfundingPlatform/Models/City/CityUpdatedModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrowdfundingPlatform.Models.City
+{
+    public class CityUpdatedModel : CityModel
+    {
+        public int CountryId { get; set; }
+    }
+}
