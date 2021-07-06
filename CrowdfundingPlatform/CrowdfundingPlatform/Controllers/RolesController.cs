4eeba93be1755103b5e482be5fc562bd785e658e
Sergey Tsarionok <s.tsarionok.vironit@vironit.ru>
2021-07-06 21:32:54 +0300

    add User roles logic

---
+++ CrowdfundingPlatform/CrowdfundingPlatform/Controllers/RolesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrowdfundingPlatform.Models.Role;
+using DataAccessLayer.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrowdfundingPlatform.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        RoleManager<IdentityRole> _roleManager;
+        UserManager<User> _userManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    return Ok(name);
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return Ok(name);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult result = await _roleManager.DeleteAsync(role);
+                return Ok();
+            }
+            else
+            {
+                ModelState.AddModelError("error", $"Role {role.Name} is not exist");
+                return BadRequest(ModelState);
+            }
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> Edit(string userId)
+        {
+            User user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var allRoles = _roleManager.Roles.ToList();
+                RoleModel model = new RoleModel
+                {
+                    UserId = user.Id,
+                    UserEmail = user.Email,
+                    UserRoles = userRoles,
+                    AllRoles = allRoles
+                };
+                return Ok(model);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Edit(string userId, List<string> roles)
+        {
+            User user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+                var addedRoles = roles.Except(userRoles);
+
+                var removedRoles = userRoles.Except(roles);
+
+                await _userManager.AddToRolesAsync(user, addedRoles);
+
+                await _userManager.RemoveFromRolesAsync(user, removedRoles);
+
+                return Ok(user);
+            }
+
+            return NotFound();
+        }
+    }
+}
